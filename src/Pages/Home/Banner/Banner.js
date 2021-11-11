64af83c189303a814e0aca4fef0a0614d0e2f282
FazleRabbiRana <ranafazrabbi@gmail.com>
2021-11-11 07:48:42 +0600

    home banner added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/swiper.min.css';
+import banner1 from '../../../images/banner/banner-bg-1.jpg';
+import banner2 from '../../../images/banner/banner-bg-2-1.jpg';
 
 const Banner = () => {
 	return (
-		<section id="home_banner">
-			This is Banner
+		<section id="home_banner" className="h-screen pt-16 md:pt-20">
+			<Swiper
+				spaceBetween={0}
+				slidesPerView={1}
+				onSlideChange={() => console.log('slide change')}
+				onSwiper={swiper => console.log(swiper)}
+				className="h-full"
+			>
+				<SwiperSlide style={{backgroundImage: `url(${banner1})`}} className="bg-no-repeat bg-cover bg-center">
+					<div className="container pt-4">
+						Slide 1
+					</div>
+				</SwiperSlide>
+				<SwiperSlide style={{backgroundImage: `url(${banner2})`}} className="bg-no-repeat bg-cover bg-center">
+					<div className="container pt-4">
+						Slide 2
+					</div>
+				</SwiperSlide>
+			</Swiper>
 		</section>
 	);
 };
--- src/Pages/Shared/Header/HeaderNavbar/HeaderNavbar.js
+++ src/Pages/Shared/Header/HeaderNavbar/HeaderNavbar.js
@@ -6,8 +6,8 @@ import MobileMenu from '../MobileMenu/MobileMenu';
 
 const HeaderNavbar = () => {
 	return (
-		<header id="header_main" className="navbar py-3 sm:py-4 lg:py-6 relative">
-			<div className="container flex justify-between">
+		<header id="header_main" className="navbar py-3 sm:py-4 absolute top-0 left-0 w-full z-30 bg-white">
+			<div className="container flex justify-between relative">
 				<div className="logo-wrapper flex-shrink-0 w-28 md:w-36">
 					<Link to="/home">
 						<img src={logo} alt="Elite Carz logo" className="w-full" />
