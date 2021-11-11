a65bb8d524baa0def6d8368b76f2e2c36ac7d659
FazleRabbiRana <ranafazrabbi@gmail.com>
2021-11-11 19:21:52 +0600

    banner slider added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,31 +1,65 @@
 import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.min.css';
-import 'swiper/swiper.min.css';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./Banner.css";
 import banner1 from '../../../images/banner/banner-bg-1.jpg';
 import banner2 from '../../../images/banner/banner-bg-2-1.jpg';
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
+	const settings = {
+		dots: true,
+		arrows: false,
+		fade: true,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		speed: 700,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		cssEase: "linear",
+		pauseOnHover: true,
+	};
+
 	return (
-		<section id="home_banner" className="h-screen pt-16 md:pt-20">
-			<Swiper
-				spaceBetween={0}
-				slidesPerView={1}
-				onSlideChange={() => console.log('slide change')}
-				onSwiper={swiper => console.log(swiper)}
-				className="h-full"
-			>
-				<SwiperSlide style={{backgroundImage: `url(${banner1})`}} className="bg-no-repeat bg-cover bg-center">
-					<div className="container pt-4">
-						Slide 1
+		<section id="home_banner" className="home-banner">
+			<Slider {...settings} className="home-banner-slider">
+				<div className="single-slide h-screen pt-16 md:pt-20">
+					<div style={{backgroundImage: `url(${banner1})`}} className="h-full bg-no-repeat bg-cover bg-center flex items-center">
+						<div className="container py-8 mb-12 md:w-3/5 lg:w-1/2 md:ml-0 md:text-center font-my-title">
+							<div className="inline-block text-left">
+								<p className="mb-2 text-blue-400 text-2xl tracking-wider">Best Deal {new Date().getFullYear()}</p>
+								<h3 className="text-white uppercase text-2xl md:text-3xl my-1">Elegant-Decorative</h3>
+								<h2 className="text-white uppercase font-bold text-4xl md:text-6xl">
+									The <span className="text-my-primary">Elite Carz</span>
+								</h2>
+								<p className="mt-2 text-white font-light text-2xl tracking-wide">True Elite Experience</p>
+								<div className="mt-8 md:mt-12">
+									<Link to="/all-products" className="btn-regular">Explore more</Link>
+								</div>
+							</div>
+						</div>
 					</div>
-				</SwiperSlide>
-				<SwiperSlide style={{backgroundImage: `url(${banner2})`}} className="bg-no-repeat bg-cover bg-center">
-					<div className="container pt-4">
-						Slide 2
+				</div>
+				<div className="single-slide h-screen pt-16 md:pt-20">
+					<div style={{backgroundImage: `url(${banner2})`}} className="h-full bg-no-repeat bg-cover bg-center flex items-center">
+						<div className="container py-8 mb-12 md:w-3/5 lg:w-1/2 md:ml-0 md:text-center font-my-title">
+							<div className="inline-block text-left">
+								<p className="mb-2 text-blue-400 text-2xl tracking-wider">Your Best Choice</p>
+								<h2 className="text-white uppercase font-bold text-4xl md:text-6xl">
+									The <span className="text-my-primary">Elite Carz</span>
+								</h2>
+								<h3 className="text-white uppercase text-2xl md:text-3xl my-1">Trustworthy-Secure</h3>
+								<p className="mt-2 text-white font-light text-2xl tracking-wide">Feel The Power</p>
+								<div className="mt-8 md:mt-12">
+									<Link to="/all-products" className="btn-regular">Explore more</Link>
+								</div>
+							</div>
+						</div>
 					</div>
-				</SwiperSlide>
-			</Swiper>
+				</div>
+			</Slider>
 		</section>
 	);
 };
--- src/Pages/Shared/Header/MainMenu/MainMenu.js
+++ src/Pages/Shared/Header/MainMenu/MainMenu.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const MainMenu = () => {
+	const navLinkActiveStyle = {
+		color: 'var(--clr-primary-dark)'
+	}
+
 	return (
 		<ul className="h-full flex flex-col md:flex-row md:items-center md:justify-end md:pl-4 space-y-2 md:space-y-0 md:space-x-8 font-my-title uppercase font-semibold text-xl md:text-base text-gray-300 md:text-my-dark-gray tracking-wide md:tracking-my-tiny">
 			<li>
-				<NavLink to="/home" className="hover:text-my-primary-dark">
+				<NavLink to="/home" activeStyle={navLinkActiveStyle} className="hover:text-my-primary-dark">
 					Home
 				</NavLink>
 			</li>
 			<li>
-				<NavLink to="/all-products" className="hover:text-my-primary-dark">
+				<NavLink to="/all-products" activeStyle={navLinkActiveStyle} className="hover:text-my-primary-dark">
 					All Products
 				</NavLink>
 			</li>
